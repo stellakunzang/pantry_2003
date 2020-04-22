52981ca9a5d7f0480f39ab526deb0d357b526e69
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-22 09:13:34 -0600

    Add test for stock attribute

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -10,4 +10,18 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
   end
-end 
+
+  def test_it_has_stock
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_ingredient_stock
+  end
+
+  def test_it_can_restock
+  end 
+
+end
