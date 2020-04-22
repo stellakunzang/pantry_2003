901dba6ea8d63fd087da0af1743fad55322a4d32
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-22 10:55:07 -0600

    Begin summary method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -3,7 +3,7 @@ require "Date"
 class CookBook
 
   attr_reader :recipes,
-              :date 
+              :date
 
   def initialize
     @recipes = []
@@ -26,6 +26,19 @@ class CookBook
     @recipes.max_by do |recipe|
       recipe.total_calories
     end
+  end
+
+  def summary
+    summary = []
+    indiv_recipe = Hash.new { |hash, key| hash[key] = {:name => nil, :details => {} } }
+
+    @recipes.each do |recipe|
+      indiv_recipe[:name] = recipe.name
+      indiv_recipe[:details] = Hash.new { |hash, key| hash[key] = {ingredients: [recipe.ingredients_required], total_calories: recipe.total_calories }}
+      summary << indiv_recipe
+      indiv_recipe = {}
+    end
+    summary
   end
 
 end
