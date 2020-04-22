c5f7c6bbb75d345b5ec0f72294e03e0ae58909e3
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-22 09:53:58 -0600

    Add test for ingredients to cookbook class

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -24,4 +24,22 @@ class CookBookTest < Minitest::Test
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
+
+  def test_it_can_ingredients
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], cookbook.ingredients
+  end
 end
