e2177831ad9d3d8c3d5241443e88409404d4f738
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-22 09:11:35 -0600

    Add attributes and attr_reader to ingredient class

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,7 +1,13 @@
 class Ingredient
+  
+  attr_reader :name,
+              :unit,
+              :calories
 
   def initialize(info)
-
+    @name = info[:name]
+    @unit = info[:unit]
+    @calories = info[:calories]
   end
-  
+
 end
