46b9530b0c3907952d516ccf695f496929343e7b
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-22 09:44:15 -0600

    Add test for attributes

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,6 +7,11 @@ class CookBookTest < Minitest::Test
 
   def test_it_exists
     cookbook = CookBook.new
-    assert_instance_of CookBook, cookbook 
+    assert_instance_of CookBook, cookbook
+  end
+
+  def test_it_has_attributes
+    cookbook = CookBook.new
+    assert_equal [], cookbook.recipes 
   end
 end
