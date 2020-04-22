238ad96778bc72213b148d4fac9edf4c9e68c88e
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-22 09:43:26 -0600

    Test it exists

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -1,15 +1,12 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "./lib/cook_book"
 require "pry"
-require "./lib/recipe"
-require './lib/ingredient'
-require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
 
   def test_it_exists
     cookbook = CookBook.new
-    assert_instance_of cookbook, Cookbook
+    assert_instance_of CookBook, cookbook 
   end
-
 end
